a16b343404e72adca029a716096d708966e58502
najibavaliyeva <ncbvlyv2003b@gmail.com>
2026-01-22 01:31:40 +0400

    Enum

---
+++ Amazon/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amazon.Models
+{
+    internal class Category
+    {
+    }
+}
